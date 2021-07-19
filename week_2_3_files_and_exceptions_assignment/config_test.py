5af30f1e955bca1010f765781ab22239038a9b97
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2021-07-19 19:17:25 -0400

    Changes to some functions, documentation added

--- week_2_3_files_and_exceptions_assignment/config_test.py
+++ week_2_3_files_and_exceptions_assignment/config_test.py
@@ -4,18 +4,16 @@ from functions.config_functions import *
 
 config_data = load_file("text_files/config_override.json", "text_files/basic_config.json")
 
-print("Safe Mode - " + config_data['Safe Mode'])
-print("Memory - " + config_data['Memory'])
-print("Error Log - " + config_data['Error Log'])
+print("Safe Mode - \"" + config_data['Safe Mode'] + "\"")
+print("Memory - \"" + config_data['Memory'] + "\"")
+print("Error Log - \"" + config_data['Error Log'] + "\"")
 
 #Prompts user asking them whether they want to make changes to the configuration or not.
 modify_config = continue_task("modify or add items to the configuration")
 
 if modify_config:
-    #If the user has already created a config_override file previously, that config is backed up with a timestamp.
-    create_backup("text_files/config_override.json")
-
-    #Prompts user to enter various configuration information. After all information is entered, writes entered information to config_override.json.
+    #Prompts user to enter various configuration information
     new_config = create_config()
 
+    #Saves the config information to config_override. If the user has already created a config_override file previously, that config is backed up with a timestamp.
     save_changes(new_config, "text_files/config_override.json")
--- week_2_3_files_and_exceptions_assignment/functions/config_functions.py
+++ week_2_3_files_and_exceptions_assignment/functions/config_functions.py
@@ -28,6 +28,40 @@ def continue_task(passed_variable):
     elif add_more.upper() == "N":
         return False
 
+def create_config():
+    """Asks user to enter various configuration details.
+    Returns: 
+        new_config -- a dictionary contaning all of the new config information
+    """
+
+    #Prompts user to enter various configuration details
+    print("\nBasic configuration options\n")
+    safe_mode = input("Safe Mode: ")
+    memory = input("Memory: ")
+    error_log = input("Error Log Location: ")
+
+    print("\nAdditional configuration options. If you would like an option removed, leave it blank.\n")
+    allow_file_uploads = input("Allow File Uploads: ")
+    use_caching = input("Use Caching: ")
+    caching_file = input("Caching File Location: ")
+    mail_host = input("Mail Host: ")
+
+    #Creates a dictionary with all the configuration options entered by the user
+    new_config = {'Safe Mode': safe_mode, 'Memory': memory, 'Error Log': error_log, 'Allow File Uploads': allow_file_uploads, 
+    'Use Caching': use_caching, 'Caching File': caching_file, 'Mail Host': mail_host}
+
+    #If any additional options were left blank, they are removed from the dictionary
+    if not allow_file_uploads:
+        new_config.pop('Allow File Uploads')
+    if not use_caching:
+        new_config.pop('Use Caching')
+    if not caching_file:
+        new_config.pop('Caching File')
+    if not mail_host:
+        new_config.pop('Mail Host')
+    
+    return new_config
+
 def load_file(first_file, second_file):
     """Loads one of two json files depending on whether the first file exists or not.
     Arguments:
@@ -46,51 +80,13 @@ def load_file(first_file, second_file):
     
     return loaded_data
 
-def create_backup(file):
-    """Creates a backup for a specified file.
-    Arguments:
-        file -- the file being backed up
-    """
-    if os.path.isfile(file):
-        shutil.copy2(file, file + ".backup" + str(time.time()))
-
-def create_config():
-    """Asks user to enter various configuration details.
-    Returns: 
-        new_config -- a dictionary contaning all of the new config information
-    """
-    print("\nModifications to basic configuration\n")
-    safe_mode = input("Safe Mode: ")
-    memory = input("Memory: ")
-    error_log = input("Error Log Location: ")
-
-    print("\nAdditional configuration options. If you would like an option removed, leave it blank.\n")
-    allow_file_uploads = input("Allow File Uploads: ")
-    use_caching = input("Use Caching: ")
-    caching_file = input("Caching File Location: ")
-    mail_host = input("Mail Host: ")
-
-    new_config = {'Safe Mode': safe_mode, 'Memory': memory, 'Error Log': error_log, 'Allow File Uploads': allow_file_uploads, 
-    'Use Caching': use_caching, 'Caching File': caching_file, 'Mail Host': mail_host}
-
-    if not allow_file_uploads:
-        new_config.pop('Allow File Uploads')
-    if not use_caching:
-        new_config.pop('Use Caching')
-    if not caching_file:
-        new_config.pop('Caching File')
-    if not mail_host:
-        new_config.pop('Mail Host')
-    
-    return new_config
-
 def save_changes(data, file):
     """Asks a user if they would like to save data to a json file.
     Arguments:
         data -- the data being saved to the file
         file -- a json file
     """
-    save_changes = input("Would you like to save your changes? (Y/N): ")
+    save_changes = input("\nWould you like to save your changes? (Y/N): ")
 
     #Checks if Y or N was entered, prompting user to enter again if not
     correct_letter = False
@@ -101,8 +97,11 @@ def save_changes(data, file):
         else:
             save_changes = input("Invalid response. Answer with either Y or N: ")
     
-    #Returns true if Y is entered and false is N is entered
-    if save_changes:
+    #Saves changes if Y is entered and discards them if N is entered. If changes are saved and the file exists already, creates a backup file.
+    if save_changes.upper() == "Y":
+        if os.path.isfile(file):
+            shutil.copy2(file, file + ".backup" + str(time.time()))
+
         with open(file, 'w') as json_obj:
             json.dump(data, json_obj)
             print("Your changes have been saved.")
