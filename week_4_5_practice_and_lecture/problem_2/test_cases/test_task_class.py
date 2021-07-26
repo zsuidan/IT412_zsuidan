79f3ffb37bd315520428ffda8c753fbb00a486e1
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2021-07-25 22:16:41 -0400

    Removed unnecessary code from Task test case

--- week_4_5_practice_and_lecture/problem_2/test_cases/test_task_class.py
+++ week_4_5_practice_and_lecture/problem_2/test_cases/test_task_class.py
@@ -9,30 +9,25 @@ class TestTaskClass(unittest.TestCase):
 
     def test_increase_time(self):
         """Tests increasing time by a set amount"""
-        self.test_task.task_time = 15
         self.test_task.increase_time(5)
         self.assertEquals(self.test_task.task_time, 20)
 
     def test_increase_time_default(self):
         """Tests increasing time by the default amount of 1"""
-        self.test_task.task_time = 15
         self.test_task.increase_time()
         self.assertEquals(self.test_task.task_time, 16)
 
     def test_decrease_time(self):
         """Tests decreasing time by a set amount"""
-        self.test_task.task_time = 15
         self.test_task.decrease_time(5)
         self.assertEquals(self.test_task.task_time, 10)
 
     def test_decrease_time_default(self):
         """Tests increasing time by the default amount of 1"""
-        self.test_task.task_time = 15
         self.test_task.decrease_time()
         self.assertEquals(self.test_task.task_time, 14)
     
     def test_reset_time(self):
         """Tests resetting the time to 0"""
-        self.test_task.task_time = 15
         self.test_task.reset_time()
         self.assertEquals(self.test_task.task_time, 0)
