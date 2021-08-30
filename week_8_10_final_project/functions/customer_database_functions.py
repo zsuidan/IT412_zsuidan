938cc6599f0d12e509b29e3934abdf0d4315b2c1
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2021-08-30 10:59:35 -0400

    Validation and test cases

--- week_8_10_final_project/functions/customer_database_functions.py
+++ week_8_10_final_project/functions/customer_database_functions.py
@@ -91,15 +91,51 @@ def view_database(database):
 
 def add_record(database):
     f_name = input("First name: ")
+
+    while not validName(f_name):
+        f_name = input("Invalid name. Please try again: ")
+
     l_name = input("Last name: ")
+
+    while not validName(f_name):
+        l_name = input("Invalid name. Please try again: ")
+
     address = input("Address: ")
+
+    while not validAddress(address):
+        address = input ("Invalid address. Please try again: ")
+
     city = input("City: ")
-    state = input("State: ")
+
+    while not validCity(city):
+        city = input("Invalid city. Please try again: ")
+
+    state = input("State: ").upper()
+
+    while not validState(state):
+        state = input("Invalid state. Please try again: ").upper()
+
     zip = input("ZIP: ")
+
+    while not validZIP(zip):
+        zip = input("Invalid ZIP code. Please try again: ")
+
     company = input("Company (optional): ")
+
     primary_phone = input("Primary phone: ")
+
+    while not primary_phone or validPhone(primary_phone):
+        primary_phone = input("Invalid phone number. Please try again: ")
+
     secondary_phone = input("Secondary phone (optional): ")
+
+    while not validPhone(secondary_phone):
+        secondary_phone = input("Invalid phone number. Please try again: ")
+
     email_address = input("Email address (optional): ")
+
+    while not validEmail(email_address):
+        email_address = input("Invalid email. Please try again: ")
 
     database.executeQuery("INSERT INTO mailings (name, company, address) VALUES ('" + f_name + " " + l_name + "','" + company + "','" + address + "')")
 
@@ -151,3 +187,72 @@ def delete_record(database):
         print("Record deleted successfully")
     else:
         print("Record deletion cancelled")
+
+def validName(name):
+    name_ok = True
+
+    if name and not name.isspace():
+        for character in name:
+            if not (character.isalpha() or character == "'" or character == "-" or character == " "):
+                name_ok = False
+    else:
+        name_ok = False
+    
+    return name_ok
+
+def validAddress(address):
+    invalid_characters = ["!", "\"", "'", "@", "$", "%", "^", "&", "*", "_", "=", "+", "<", ">", "?", ";", "[", "]", "{", "}"]
+    address_ok = True
+
+    if address and not address.isspace():
+        for character in address:
+            if character in invalid_characters:
+                address_ok = False
+    else:
+        address_ok = False
+
+    return address_ok
+
+def validCity(city):
+    city_ok = True
+
+    if city and not city.isspace():
+        for character in city:
+            if not (character.isalpha() or character == "'" or character == " "):
+                city_ok = False
+    else:
+        city_ok = False
+
+    return city_ok
+
+def validState(state):
+    if state.isalpha() and len(state) == 2 and state.isupper():
+        return True
+    else:
+        return False
+
+def validZIP(zip):
+    if zip.isnumeric() and (len(zip) == 4 or len(zip) == 5):
+        return True
+    else:
+        return False
+
+def validPhone(phone):
+    phone_ok = True
+
+    for character in phone:
+        if not (character.isnumeric() or character == "-"):
+            phone_ok = False
+
+    return phone_ok
+
+def validEmail(email):
+    invalid_characters = ["!", "\"", "'", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", ",", "<", ">", "/", "?", ";", ":", "[", "]", "{", "}", "\\", " "]
+
+    email_ok = True
+
+    for character in email:
+        if character in invalid_characters:
+            email_ok = False
+
+    return email_ok
---
+++ week_8_10_final_project/test_cases/customer_database_test.py
@@ -0,0 +1,88 @@
+import unittest
+from functions.customer_database_functions import *
+
+class TestCustomerDatabaseFunctions(unittest.TestCase):
+    """Tests functions in customer_database_functions.py"""
+    def test_valid_name_success(self):
+        valid_names_to_test = ["Zach", "Za-ch", "Za'ch"]
+
+        for name in valid_names_to_test:
+            self.assertTrue(validName(name))
+
+    def test_valid_name_failure(self):
+        invalid_names_to_test = ["Za%#$ch", "Z4ch", "", " "]
+
+        for name in invalid_names_to_test:
+            self.assertFalse(validName(name))
+
+    def test_valid_address_success(self):
+        valid_addresses_to_test = ["123 ABC Street", ",", "-", "123", "ABC"]
+
+        for address in valid_addresses_to_test:
+            self.assertTrue(validAddress(address))
+
+    def test_valid_address_failure(self):
+        invalid_addresses_to_test = ["", " ", "!", "\"", "'", "@", "$", "%", "^", "&", "*", "_", "=", "+", "<", ">", "?", ";", "[", "]", "{", "}"]
+
+        for address in invalid_addresses_to_test:
+            self.assertFalse(validAddress(address))
+
+    def test_valid_city_success(self):
+        valid_cities_to_test = ["City", "Ci ty", "Ci'ty", "city", "CITY"]
+
+        for city in valid_cities_to_test:
+            self.assertTrue(validCity(city))
+
+    def test_valid_city_failure(self):
+        invalid_cities_to_test = ["", " ", "!@#", "City!@#%", "123", "City123"]
+
+        for city in invalid_cities_to_test:
+            self.assertFalse(validCity(city))
+
+    def test_valid_state_success(self):
+        valid_states_to_test = ["MI", "AZ", "NY"]
+
+        for state in valid_states_to_test:
+            self.assertTrue(validState(state))
+
+    def test_valid_state_failure(self):
+        invalid_states_to_test = ["ABC", "A", "", " ", "12", "!@", "mi"]
+
+        for state in invalid_states_to_test:
+            self.assertFalse(validState(state))
+
+    def test_valid_zip_success(self):
+        valid_zips_to_test = ["1234", "12345"]
+
+        for zip in valid_zips_to_test:
+            self.assertTrue(validZIP(zip))
+
+    def test_valid_zip_failure(self):
+        invalid_zips_to_test = ["123", "123456", "abcd", "abcde", "!@#$", "!@#$%", "", " "]
+
+        for zip in invalid_zips_to_test:
+            self.assertFalse(validZIP(zip))
+
+    def test_valid_phone_success(self):
+        valid_phones_to_test = ["1234567890", "123-456-7890", ""]
+
+        for phone in valid_phones_to_test:
+            self.assertTrue(validPhone(phone))
+
+    def test_valid_phone_failure(self):
+        invalid_phones_to_test = ["123abcd", "123 456", "abc", "!@#", "123$^%"]
+
+        for phone in invalid_phones_to_test:
+            self.assertFalse(validPhone(phone))
+
+    def test_valid_email_success(self):
+        valid_emails_to_test = ["bob@gmail.com", ""]
+
+        for email in valid_emails_to_test:
+            self.assertTrue(validEmail(email))
+
+    def test_valid_email_failure(self):
+        invalid_emails_to_test = ["bob @ gmail.com", "!", "\"", "'", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", ",", "<", ">", "/", "?", ";", ":", "[", "]", "{", "}", "\\", " "]
+
+        for email in invalid_emails_to_test:
+            self.assertFalse(validEmail(email))
